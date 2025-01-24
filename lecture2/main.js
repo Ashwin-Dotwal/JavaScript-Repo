da99d6eb375d7b2b2b863d8e6d5924185b5456f6
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-01-24 12:51:53 +0530

    add new update

--- lecture2/main.js
+++ lecture2/main.js
@@ -1,1 +1,44 @@
-console.log("hello jii");
+//var variabale 
+
+var age= 25;
+
+
+if(true){
+  console.log(age);
+}
+
+//var variable in function
+
+function solve(){
+  var age=24;
+  console.log(age);
+}
+solve();
+
+//let variable 
+
+let a=10;
+
+console.log(a);
+
+//let variable is blocked scop variable
+{
+  let a=52;
+  console.log(a);
+}
+
+// const variable
+
+const size=29;
+console.log(size);
+
+//data types in js
+
+//primitive data types
+
+//number data typer num
+
+let marks=63.3365;
+marks = "ashwin";
+
+console.log(marks);
