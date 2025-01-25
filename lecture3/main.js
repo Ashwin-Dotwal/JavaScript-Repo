3f106d7665ecd3ecf5645fc7eba22a8a2ebcc828
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-01-25 22:58:07 +0530

    add new file

--- lecture3/main.js
+++ lecture3/main.js
@@ -61,4 +61,25 @@ else if(number==3){
 
 else{
   console.log('number is not present');
+} 
+
+// switch case
+
+let num =2;
+ 
+switch(num){
+  case 1: console.log('A');
+  break;
+
+  case 2: console.log('B');
+  break;
+
+  case 3: console.log('C');
+  break;
+
+  case 4: console.log('D');
+  break;
+
+  default: console.log('A');
+  break;
 }
