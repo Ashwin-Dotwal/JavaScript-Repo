1465e248f3c1ae1c4082c2d5f837725becd730da
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-02-01 23:07:38 +0530

    add new file

---
+++ lecture10/Date-in-build-object.js
@@ -0,0 +1,20 @@
+//DAte in build object
+
+let curr=new Date();
+
+console.log(curr);
+
+let date=new Date('sep 25 2002 05:15');
+console.log(date);
+
+let newDate=new Date(2001,8,25,7);
+
+//console.log(newDate);
+
+console.log(newDate.getDay());
+
+console.log(newDate.getFullYear());
+
+console.log(newDate.setFullYear(2005));
+
+console.log(newDate);
---
+++ lecture10/math-in-build-object.js
@@ -0,0 +1,20 @@
+//in build objects in js
+
+//math in build object
+
+console.log("ashwin");
+
+console.log(Math.PI);
+
+console.log(Math.max(50,662,85,222,689));
+
+console.log(Math.min(25,58,10,965));
+
+console.log(Math.round(1.8));
+
+console.log(Math.floor(1.9));
+
+console.log(Math.ceil(1.3));
+
+console.log(Math.abs(-8));
+
