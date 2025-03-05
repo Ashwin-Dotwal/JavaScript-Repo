08b156bfbff132f7a3a993dc4c7b8911fdcad5c0
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-03-05 11:29:37 +0530

    add new file

---
+++ lecture14/domincss.js
@@ -0,0 +1,9 @@
+let paraelement=document.getElementById('spara');
+
+paraelement.style.backgroundColor='blue';
+paraelement.style.color='white';
+
+
+let secondElement=document.getElementById('sdiv');
+
+secondElement.style.cssText="background-color:black;color:white;padding:0.5rem"
