9423ab60e51abc6fa962cb050714b5c3063a3548
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-10-08 11:08:23 +0530

    add some code

---
+++ HostingInJavaScript/var.js
@@ -0,0 +1,11 @@
+getname()
+console.log(x);
+
+var x=7;
+function getname(){
+  console.log("Namaste Javascript");
+}
+
+
+
+console.log("Hello From External Javascript");
---
+++ lecture15/eventandlisnersinjs.js
@@ -0,0 +1,10 @@
+
+
+function changeText(){
+let fpara=document.getElementById('fpara');
+fpara.textContent="Hello Ashwin"
+}
+
+let fpara=document.getElementById('fpara');
+
+fpara.addEventListener('click',changeText);
